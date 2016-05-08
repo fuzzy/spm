1a28e927d6303837bc4d0001a5279e25a3b15588
Mike 'Fuzzy' Partin <fuzzy@fumanchu.org>
2016-05-07 19:25:45 -0700

    code cleanup, and most of ftp support done

--- tools/goget/goget.go
+++ tools/goget/goget.go
@@ -3,12 +3,24 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"github.com/dutchcoders/goftp"
 	"io"
 	"net/http"
 	"os"
 	"strings"
 )
 
+var Insecure = false
+var Quiet = false
+var Uri = ""
+var Out = ""
+
+/*
+ Function: ErrChk(error)
+ Arguments: e
+ Notes: It calls os.Exit(1) if triggered.
+ Author: Mike 'Fuzzy' Partin
+*/
 func ErrChk(e error) {
 	if e != nil {
 		fmt.Println(e)
@@ -16,11 +28,117 @@ func ErrChk(e error) {
 	}
 }
 
+/*
+ Function: HttpGet(string, *os.File)
+ Arguments:
+*/
+func HttpGet(u string) {
+	// create our http response object
+	resp, err := http.Get(u)
+	ErrChk(err)
+	defer resp.Body.Close()
+
+	// create our output file
+	out, err := os.Create(Out)
+	ErrChk(err)
+	defer out.Close()
+
+	// and download the file
+	_, err = io.Copy(out, resp.Body)
+	ErrChk(err)
+}
+
+/*
+
+ */
+func HttpsGet(u string, i bool) {
+	// create our https support
+	tr := &http.Transport{}
+	if !i {
+		tr = &http.Transport{
+			TLSClientConfig:    &tls.Config{},
+			DisableCompression: true,
+		}
+	} else {
+		tr = &http.Transport{
+			TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
+			DisableCompression: true,
+		}
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get(u)
+	ErrChk(err)
+	defer resp.Body.Close()
+
+	// create our output file
+	out, err := os.Create(Out)
+	ErrChk(err)
+	defer out.Close()
+
+	// and dload it
+	_, err = io.Copy(out, resp.Body)
+	ErrChk(err)
+}
+
+func parseFtpUri(u string) map[string]string {
+	s := strings.Split(Uri, "/")
+	ftpData := make(map[string]string)
+	// if we have a ':' we at the least have a port definition
+	if strings.Contains(s[2], ":") {
+		// additionally if we have an '@' at this point, we have, at the least login info
+		if strings.Contains(s[2], "@") {
+			c := strings.Split(s[2], ":")
+			a := strings.Split(c[0], "@")
+			ftpData["Host"] = c[1]
+			ftpData["User"] = a[0]
+			ftpData["Pswd"] = a[1]
+			if len(c) == 3 {
+				ftpData["Port"] = c[2]
+			} else {
+				ftpData["Port"] = "21"
+			}
+		} else {
+			ftpData["User"] = "ftp"
+			ftpData["Pswd"] = "user@domain.tld"
+			// well we still have a port anyway.
+			ftpData["Host"] = strings.Split(s[2], ":")[0]
+			ftpData["Port"] = strings.Split(s[2], ":")[1]
+		}
+	} else {
+		ftpData["Host"] = s[2]
+		ftpData["Port"] = "21"
+		ftpData["User"] = "ftp"
+		ftpData["Pswd"] = "user@domain.tld"
+	}
+	ftpData["Path"] = strings.Join(s[3:len(s)-1], "/")
+	ftpData["File"] = s[len(s)-1]
+	ftpData["Oput"] = Out
+	return ftpData
+}
+
+func FtpGet(s string) {
+	data := parseFtpUri(s)
+	var ftp *goftp.FTP
+	var err error
+
+	fmt.Println(data)
+
+	ftp, err = goftp.Connect(fmt.Sprintf("%s:%s", data["Host"], data["Port"]))
+	ErrChk(err)
+	defer ftp.Close()
+
+	ErrChk(ftp.Login(data["User"], data["Pswd"]))
+	ErrChk(ftp.Cwd(data["Path"]))
+
+	// create our output file
+	out, err := os.Create(Out)
+	ErrChk(err)
+	defer out.Close()
+
+	//ErrChk(ftp.Retr)
+}
+
 func main() {
-	var Insecure = false
-	var Quiet = false
-	var Uri = ""
-	var Out = ""
 	if len(os.Args) > 1 {
 		for i := 1; i < len(os.Args); i++ {
 			if os.Args[i] == "-h" || os.Args[i] == "-help" || os.Args[i] == "--help" {
@@ -54,52 +172,28 @@ func main() {
 				}
 			}
 		}
-		if !Quiet {
-			// Create our output filehandle
-			if f, _ := os.Stat(Out); f.IsDir() {
-				data := strings.Split(Uri, "/")
-				turi := fmt.Sprintf("%s/%s", Out, data[len(data)-1])
-				Out = turi
-			}
-			out, err := os.Create(Out)
-			ErrChk(err)
-			defer out.Close()
+		// Create our output filehandle
+		if Out == "" {
+			Out = "/tmp"
+		}
+		if f, _ := os.Stat(Out); f.IsDir() {
+			data := strings.Split(Uri, "/")
+			turi := fmt.Sprintf("%s/%s", Out, data[len(data)-1])
+			Out = turi
+		}
 
-			// And do the dirty work
-			if Uri[:5] == "ftp:/" {
-				fmt.Println("FTP Support is being worked on")
-			} else {
-				if Uri[:5] == "http:" {
-					// create our http response object
-					resp, err := http.Get(Uri)
-					ErrChk(err)
-					defer resp.Body.Close()
-					// and download the file
-					_, err = io.Copy(out, resp.Body)
-					ErrChk(err)
-				} else if Uri[:5] == "https" {
-					// create our https support
-					tr := &http.Transport{}
-					if !Insecure {
-						tr = &http.Transport{
-							TLSClientConfig:    &tls.Config{},
-							DisableCompression: true,
-						}
-					} else {
-						tr = &http.Transport{
-							TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
-							DisableCompression: true,
-						}
-					}
-					client := &http.Client{Transport: tr}
-					resp, err := client.Get(Uri)
-					ErrChk(err)
-					defer resp.Body.Close()
-					// and dload it
-					_, err = io.Copy(out, resp.Body)
-					ErrChk(err)
-				}
+		// And do the dirty work
+		if Uri[:5] == "ftp:/" {
+			FtpGet(Uri)
+		} else {
+			if Uri[:5] == "http:" {
+				HttpGet(Uri)
+			} else if Uri[:5] == "https" {
+				HttpsGet(Uri, Insecure)
 			}
 		}
+	}
+	if Quiet {
+		fmt.Println("fuck off")
 	}
 }
